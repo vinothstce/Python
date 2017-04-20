546191565db63cb5411c1633058d74c284d30976
Vinoth <vinothstce@gmail.com>
2017-04-20 14:02:08 +0530

    Command line argument example

---
+++ google_argv.py
@@ -0,0 +1,8 @@
+import sys
+
+#Define a main function() that prints the command line arguments 
+def main():
+    print(sys.argv)
+#This is a standard boilerplate that calls the main function 
+if __name__ == '__main__':
+    main()
