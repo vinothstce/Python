585886e2e0eac9e82572f04290eed0eeffa47787
Vinoth <vinothstce@gmail.com>
2017-04-27 10:50:43 +0530

    Google Exercise Video

---
+++ google_dictionaryExec.py
@@ -0,0 +1,9 @@
+d = {1 : 2, 3 : 4, 4 : 3, 2 : 1, 0 : 0}
+print 'Orginal dictionary : ', d
+
+# Getting the values in a dictionary using the key as below,
+print d.get(1)
+print d.get(3)
+
+#Getting the key in a dictionary using the values as below,
+
